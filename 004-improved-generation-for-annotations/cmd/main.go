568ed9f8613faf936c900db26da6c2f2dbf82ce9
denysprokhorin <prokhorind@gmail.com>
2025-05-23 22:25:07 +0300

    add env variable

--- 004-improved-generation-for-annotations/cmd/main.go
+++ 004-improved-generation-for-annotations/cmd/main.go
@@ -2,11 +2,17 @@ package main
 
 import (
 	"github.com/prokhorind/go_course/004-improved-generation-for-annotations/bot"
+	"log"
+	"os"
 )
 
-const token = ""
-
 func main() {
+
+	token := os.Getenv("TELEGRAM_BOT_TOKEN")
+	if token == "" {
+		log.Fatal("TELEGRAM_BOT_TOKEN is not set")
+	}
+
 	engine := bot.NewEchotronBot(token)
 	engine.Run()
 }
